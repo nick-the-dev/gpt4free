3e08eaa4f06fc2c9dd79f8570fb8886702b2da2e
Nick <57038095+nick-the-dev@users.noreply.github.com>
2023-06-06 17:26:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Bot } = require('grammy');
+const axios = require('axios');
+
+const bot = new Bot('6248055605:AAGlr_Eyll1IYQMeLro0GxfYLFhOwSgIDDI');
+
+bot.command('start', (ctx) => {
+  ctx.reply('Welcome! Send me a message, and I will encode it and send a cURL request to the specified URL.');
+});
+
+bot.on('message', async (ctx) => {
+  try {
+    const inputText = ctx.message.text;
+    const encodedText = encodeURIComponent(inputText + " Remember that the maximum length of your response is 3000 chars.");
+    const url = 'http://162.243.187.205:3000/ask';
+
+    // const response = await axios.get(url, `prompt=${encodedText}`, {
+    //   headers: {
+    //     'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    // });
+
+    const response = await axios.get(url, {
+    params: {
+        'prompt': encodedText,
+        'model': 'forefront'
+        }
+    });
+
+    if (response.data.length <= 4096) {
+      ctx.reply(`${response.data}`);
+    } else {
+      ctx.reply(`Respponse is too long: ${response.data.length}`);
+    }
+  } catch (error) {
+    console.error(error);
+    ctx.reply('An error occurred while sending the cURL request.');
+  }
+});
+
+bot.start();
